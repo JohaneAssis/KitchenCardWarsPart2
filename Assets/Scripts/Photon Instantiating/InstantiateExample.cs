b4b3d4063e952c7682a46a8089b515d32bb12803
Oris Feliz <felizoris@gmail.com>
2019-12-18 14:26:25 -0500

    added some scripts that should help with multiplayer

---
+++ Assets/Scripts/Photon Instantiating/InstantiateExample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateExample : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefab;
+
+    // Update is called once per frame
+    void Awake()
+    {
+        MasterManager.NetworkInstantiate(_prefab, transform.position, Quaternion.identity);  
+    }
+}
